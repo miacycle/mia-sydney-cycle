7cd6d73e047282e3fb4b516d1a5e9113b0f6a1dc
miacycle <184569369+miacycle@users.noreply.github.com>
2025-04-17 14:01:12 -0500

    update
    Signed-off-by: miacycle <184569369+miacycle@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/mia-sydney-cycle',
   server: {
     host: "::",
     port: 8080,
